1fc51849879980be627efaab39628a4f027a153b
kusin <aryajayaalamsyah@gmail.com>
2023-03-25 03:31:54 +0700

    -

--- adminweb/media.php
+++ adminweb/media.php
@@ -72,7 +72,7 @@
 		<!-- /.content-wrapper -->
 
 		<footer class="main-footer">
-    		<div class="float-right d-none d-sm-block">Version 1.0</div>
+    		<div class="float-right d-none d-sm-block">AdminLTE 3.2.0</div>
     		Copyright &copy; 2023 Aryajaya Alamsyah, S.Kom. All rights reserved.
   		</footer>
 
