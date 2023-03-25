ea31a277f8536789afe7506fb6b4580ed83a86d7
kusin <aryajayaalamsyah@gmail.com>
2023-03-25 07:23:06 +0700

    -

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
 ## Aplikasi Pembayaran SPP
+
 - Studi case "SMK Triguna Utama"
 
+## Hak Akses Sistem
+
+- Adminstrator
+- Operator
+- Siswa
+
 ## Software Requirements
+
 - XAMPP 7.4.30
 - VSCode last update
 - Google Chrome last update
 
 ## Library Requirements
+
 - AdminLTE 3.2.0
-- Bootstrap 5.2.0
-- Fontawesome 6.3.0
+- Bootstrap 4.6.1
+- Fontawesome 6.2.0
--- adminweb/modul/mod_jurusan/jurusan.php
+++ adminweb/modul/mod_jurusan/jurusan.php
@@ -14,6 +14,7 @@
         default:
             
             echo "<div class='row'>";
+                
                 echo "<div class='col-md-12'>";
                     echo "<div class='callout callout-success'>";
                         echo "<p class='text-secondary'>Data Jurusan SMK Triguna Utama</p>";
@@ -23,7 +24,7 @@
                 
                 echo "<div class='col-md-12'>";
                     echo "
-                        <button type='button' class='btn btn-outline-info btn-md mb-2'>
+                        <button type='button' class='btn btn-outline-info btn-md mb-2' onclick=window.location.href=\"?page=jurusan&act=tambah-data\">
                             Tambah Data
                         </button>
                     ";
@@ -48,13 +49,13 @@
                                     echo "<td>$rows[singkatan]</td>";
                                     echo "<td>$rows[status_data]</td>";
                                     echo "<td>
-                                            <button type='button' class='btn btn-primary btn-sm'>
+                                            <button type='button' class='btn btn-outline-primary btn-sm'>
                                                 <i class='fa-solid fa-eye'></i>
                                             </button>
-                                            <button type='button' class='btn btn-warning btn-sm'>
+                                            <button type='button' class='btn btn-outline-warning btn-sm'>
                                                 <i class='fa-solid fa-edit'></i>
                                             </button>
-                                            <button type='button' class='btn btn-danger btn-sm'>
+                                            <button type='button' class='btn btn-outline-danger btn-sm'>
                                                 <i class='fa-solid fa-trash'></i>
                                             </button>
                                     </td>";
@@ -79,13 +80,86 @@
 
             break;
 
-        case "tambah-data";
+        case "tambah-data":
+            
+            echo "<div class='row'>";
+                
+                echo "<div class='col-md-12'>";
+                    echo "<div class='callout callout-success'>";
+                        echo "<p class='text-secondary'>Data Jurusan SMK Triguna Utama</p>";
+                    echo "</div>";
+                echo "</div>";
+                // <!-- /.col-md-12 -->
+
+                echo "<div class='col-md-12'>";
+                    echo "<div class='card'>";
+                        echo "<div class='card-body'>";
+                            echo "<form action='#' method='#'>";
+                                
+                                echo "
+                                    <div class='form-group'>
+                                        <label for'txt_nama_jurusan'>Nama jurusan</label>
+                                        <input type='text' class='form-control' id='txt_nama_jurusan' name='txt_nama_jurusan' placeholder='Nama Jurusan'>
+                                    </div>
+                                ";
+
+                                echo "
+                                    <div class='form-group'>
+                                        <label for'txt_singkatan'>Singkatan</label>
+                                        <input type='text' class='form-control' id='txt_singkatan' name='txt_singkatan' placeholder='Singkatan'>
+                                    </div>
+                                ";
+
+                                echo "
+                                    <div class='form-group'>
+                                        <label for'cb_status_data'>Status Data</label>
+                                        <select class='form-control' id='cb_status_data' name='cb_status_data'>
+                                            <option value='Aktif'>Aktif</option>
+                                            <option value='Tidak Aktif'>Tidak Aktif</option>
+                                        </select>
+                                    </div>
+                                ";
+
+                                echo "
+                                    <div class='form-group'>
+                                        <button type='submit' class='btn btn-primary'>Submit</button>
+                                    </div>
+                                ";
+                                
+                            echo "</form>";
+                            // <!-- /.form-tambah-data -->
+                        echo "</div>";
+                        // <!-- /.card-body -->    
+                    echo "</div>";
+                    // <!-- /.card -->
+                echo "</div>";
+                // <!-- /.col-md-12 -->
+
+            echo "</div>";
+            // <!-- /.row -->
+
             break;
         
-        case "ubah-data";
+        case "ubah-data":
+
+            echo "<div class='row'>";
+                
+                echo "<div class='col-md-12'>";
+                    echo "Tes 1";
+                echo "</div>";
+                // <!-- /.col-md-12 -->
+
+                echo "<div class='col-md-12'>";
+                    echo 'Tes 2';
+                echo "</div>";
+                // <!-- /.col-md-12 -->
+
+            echo "</div>";
+            // <!-- /.row -->
+
             break;
 
-        case "hapus-data";
+        case "hapus-data":
             break;
     }
 
